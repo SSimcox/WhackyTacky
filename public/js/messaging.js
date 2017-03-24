2ca7166dc44ff7289efe83c2b65c4ac28b0cbc11
IssacImmarel <steven.d.simcox@gmail.com>
2017-03-23 21:58:27 -0600

    Change one browser, change em all

--- public/js/messaging.js
+++ public/js/messaging.js
@@ -11,10 +11,9 @@ socket.on('change color',changeYourColor)
 function changeYourColor(colorString){
   var context = document.getElementById("your-canvas").getContext("2d")
   context.save()
-  context.fillStyle(colorString)
+  context.fillStyle = colorString
   context.fillRect(0,0,context.canvas.width,context.canvas.height)
   context.restore()
-  socket.emit('change color',colorString)
 }
 
 function changeMyColor(){
@@ -24,7 +23,7 @@ function changeMyColor(){
   var b = Math.floor(Math.random() * 255)
   var colorString = `rgb(${r},${g},${b})`
   context.save()
-  context.fillStyle(colorString)
+  context.fillStyle = colorString
   context.fillRect(0,0,context.canvas.width,context.canvas.height)
   context.restore()
   socket.emit('change color',colorString)
--- routes/io.js
+++ routes/io.js
@@ -5,11 +5,24 @@
 
 module.exports = function(app) {
   let io = require('socket.io')(app)
+  let connections = {}
+
   io.on('connection', function(socket){
-    console.log("Socket: ", socket);
+    console.log(`Socket: ${socket.id}`);
+    connections[socket.id] = socket
+    console.log(`Number of Clients: ${Object.keys(connections).length}`)
+
     socket.on('change color', function(color){
       socket.broadcast.emit('change color', color)
+      console.log(color)
+    })
+
+    socket.on('disconnect', function(){
+      delete connections[socket.id]
+      console.log(`Deleted ${socket.id}\n Number of Clients:  ${Object.keys(connections).length}`)
     })
   })
+
   return io
 }
+
